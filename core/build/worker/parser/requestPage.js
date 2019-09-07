f62ff318c6e3928e093a38b3cc9c767015928b95
alireza bonab <alireza.bonab@ipsos.com>
2019-09-07 20:51:55 +0200

    remove build

--- core/build/worker/parser/requestPage.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var services_1 = require("../../services");
-var Get = services_1.Request.Get;
-exports.default = (function (url) { return Get(url); });
