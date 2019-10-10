a0494a24d3279bc13d11a8b05a210c73def458a7
alireza bonab <alireza.bonab@ipsos.com>
2019-10-10 23:58:16 +0200

    refactor

--- core/src/utils/scrapper/index.ts
+++
@@ -1,6 +0,0 @@
-import { IBrowser } from "../browser/IBrowser";
-import { ArrayItem } from "../../model";
-
-export default (url: string, browser: IBrowser, scrapItem: ArrayItem) => {
-  console.log("hi");
-};
